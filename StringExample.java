66a1c10f53c8106d7183b992370b3efddc1caa25
pseud039 <saumya.2428cseaiml12@kiet.edu>
2025-09-12 14:11:46 +0530

    class 12-09

--- StringExample.java
+++ StringExample.java
@@ -1,44 +1,21 @@
 
 
-// public class StringExample{
-
-//     public static void main(String[] args) {
-//         // String name = "TITLE";
-//         // int len = name.length();
-//         // System.out.println(len);
-//         // System.out.println(name.toLowerCase());
-//         // System.out.println(name.substring(2, 3).toLowerCase());
-//         // String s = "HOLA";
-//         // if(name.equals(s)){
-//         //     System.out.println("YES");
-//         // }else{
-//         //     System.out.println("NO");
-//         // }
-//         // // Scanner sc = new Scanner();
-//         // // sc
-        
-//     }
-// }
-import java.util.*;
-
-public class StringExample {
+public class StringExample{
 
     public static void main(String[] args) {
+        String name = "TITLE";
+        int len = name.length();
+        System.out.println(len);
+        System.out.println(name.toLowerCase());
+        System.out.println(name.substring(2, 3).toLowerCase());
+        String s = "HOLA";
+        if(name.equals(s)){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+        // Scanner sc = new Scanner();
+        // sc
         
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        String B=sc.next();
-        /* Enter your code here. Print output to STDOUT. */
-        System.out.println(A.length() + B.length());
-  if(A.length() > B.length()){
-    System.out.println("Yes");
-    }
-    else{System.out.println("No");
-    };
-        A.to();        
     }
 }
-
-
-
-undefined
